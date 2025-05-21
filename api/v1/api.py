5b0949f5100f562733103fcf1547929c6fd18480
dnetochaves <dnetochaves@gmail.com>
2025-05-21 10:09:12 -0300

    Integração com microserviço autenticação do usuário no microserviço de pedidos

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from api.v1.endpoints import pedidos
+from api.v1.endpoints import pedidos, auth
 
 api_router = APIRouter()
 api_router.include_router(pedidos.router, prefix="/pedidos", tags=["pedidos"])
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
